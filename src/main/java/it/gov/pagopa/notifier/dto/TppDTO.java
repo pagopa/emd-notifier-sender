f612931b5d5e0ac991612bcacf4b34077a75eeea
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-10-30 16:19:57 +0100

    Consumer fix

--- src/main/java/it/gov/pagopa/notifier/dto/TppDTO.java
+++ src/main/java/it/gov/pagopa/notifier/dto/TppDTO.java
@@ -1,26 +1,26 @@
 package it.gov.pagopa.notifier.dto;
 
-
 import it.gov.pagopa.notifier.enums.AuthenticationType;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 public class TppDTO {
-    @NotNull
     private String tppId;
     private String entityId;
+    private String idPsp;
     private String businessName;
+    private String legalAddress;
     private String messageUrl;
     private String authenticationUrl;
     private AuthenticationType authenticationType;
     private Contact contact;
     private Boolean state;
-
-
-
+    private LocalDateTime creationDate;
+    private LocalDateTime lastUpdateDate;
 }
