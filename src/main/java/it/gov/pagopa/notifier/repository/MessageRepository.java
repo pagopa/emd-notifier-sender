8553023e6fad04d13f51c1f922e9972c3d32014a
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-11-14 11:16:59 +0100

    Add Associated Payment

--- src/main/java/it/gov/pagopa/notifier/repository/MessageRepository.java
+++ src/main/java/it/gov/pagopa/notifier/repository/MessageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
 public interface MessageRepository extends ReactiveMongoRepository<Message,String> {
-    Flux<Message> findByHashedFiscalCode(String hashedFiscalCode);
+    Flux<Message> findByRecipientId(String recipientId);
 
 }
