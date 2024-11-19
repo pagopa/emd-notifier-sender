b7f14b08572e2362a731bf36b69050d9cb9dead4
stefanodel <stefano11.delia@nttdata.com>
2024-11-19 15:32:09 +0100

    fix: update annotation on NotifyErrorQueueMessageDTO

--- src/main/java/it/gov/pagopa/notifier/dto/NotifyErrorQueueMessageDTO.java
+++ src/main/java/it/gov/pagopa/notifier/dto/NotifyErrorQueueMessageDTO.java
@@ -1,10 +1,14 @@
 package it.gov.pagopa.notifier.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class NotifyErrorQueueMessageDTO {
 
     private MessageDTO messageDTO;
