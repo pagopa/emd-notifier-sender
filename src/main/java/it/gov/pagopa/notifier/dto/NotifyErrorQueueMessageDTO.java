51563c559cceaf220159dd25be5504c0911b1fdc
stefanodel <stefano11.delia@nttdata.com>
2024-11-20 14:56:58 +0100

    Revert "fix: update annotation on NotifyErrorQueueMessageDTO"
    This reverts commit b7f14b08572e2362a731bf36b69050d9cb9dead4.

--- src/main/java/it/gov/pagopa/notifier/dto/NotifyErrorQueueMessageDTO.java
+++ src/main/java/it/gov/pagopa/notifier/dto/NotifyErrorQueueMessageDTO.java
@@ -1,14 +1,10 @@
 package it.gov.pagopa.notifier.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class NotifyErrorQueueMessageDTO {
 
     private MessageDTO messageDTO;
