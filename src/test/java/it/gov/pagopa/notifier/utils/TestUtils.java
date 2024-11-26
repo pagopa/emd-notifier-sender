075d8e4cd4b45cf315abaf6b1a384ac38ed5ad45
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-11-26 12:15:19 +0100

    Sonar maintainability fix

--- src/test/java/it/gov/pagopa/notifier/utils/TestUtils.java
+++ src/test/java/it/gov/pagopa/notifier/utils/TestUtils.java
@@ -11,21 +11,21 @@ public class TestUtils {
 
     public TestUtils(){}
 
-    public final static TppDTO TPP_DTO = TppDTOFaker.mockInstance();
-    public final static List<TppDTO> TPP_DTO_LIST = List.of(TPP_DTO);
-    public final static String TPP_ID = TPP_DTO.getTppId();
-    public final static List<String> TPP_ID_STRING_LIST = List.of(TPP_ID);
-    public final static TppIdList TPP_ID_LIST = new TppIdList(List.of(TPP_ID));
+    public static final TppDTO TPP_DTO = TppDTOFaker.mockInstance();
+    public static final List<TppDTO> TPP_DTO_LIST = List.of(TPP_DTO);
+    public static final String TPP_ID = TPP_DTO.getTppId();
+    public static final List<String> TPP_ID_STRING_LIST = List.of(TPP_ID);
+    public static final TppIdList TPP_ID_LIST = new TppIdList(List.of(TPP_ID));
     public static final MessageDTO MESSAGE_DTO = MessageDTOFaker.mockInstance();
-    public final static String FISCAL_CODE = MESSAGE_DTO.getRecipientId();
-    public static Message<String> QUEUE_MESSAGE_NO_RETRY_CORE = MessageCoreQueueFaker.mockNoRetryInstance(MESSAGE_DTO);
-    public static Message<String> QUEUE_NOTIFIER_NO_RETRY_ERROR = NotifierErrorQueueFaker.mockNoRetryInstance(MESSAGE_DTO,TPP_DTO);
-    public static Message<String> QUEUE_MESSAGE_STRING_CORE = MessageCoreQueueFaker.mockStringInstance(MESSAGE_DTO);
-    public static Message<String> QUEUE_NOTIFIER_STRING_ERROR = NotifierErrorQueueFaker.mockStringInstance(MESSAGE_DTO,TPP_DTO);
-    public static Message<MessageDTO> QUEUE_MESSAGE_CORE = MessageCoreQueueFaker.mockInstance(MESSAGE_DTO);
-    public static Message<NotifyErrorQueuePayload> QUEUE_NOTIFIER_ERROR = NotifierErrorQueueFaker.mockInstance(MESSAGE_DTO,TPP_DTO);
+    public static final String FISCAL_CODE = MESSAGE_DTO.getRecipientId();
+    public static final Message<String> QUEUE_MESSAGE_NO_RETRY_CORE = MessageCoreQueueFaker.mockNoRetryInstance(MESSAGE_DTO);
+    public static final Message<String> QUEUE_NOTIFIER_NO_RETRY_ERROR = NotifierErrorQueueFaker.mockNoRetryInstance(MESSAGE_DTO,TPP_DTO);
+    public static final Message<String> QUEUE_MESSAGE_STRING_CORE = MessageCoreQueueFaker.mockStringInstance(MESSAGE_DTO);
+    public static final Message<String> QUEUE_NOTIFIER_STRING_ERROR = NotifierErrorQueueFaker.mockStringInstance(MESSAGE_DTO,TPP_DTO);
+    public static final Message<MessageDTO> QUEUE_MESSAGE_CORE = MessageCoreQueueFaker.mockInstance(MESSAGE_DTO);
+    public static final Message<NotifyErrorQueuePayload> QUEUE_NOTIFIER_ERROR = NotifierErrorQueueFaker.mockInstance(MESSAGE_DTO,TPP_DTO);
 
-    public static NotifyErrorQueuePayload NOTIFIER_ERROR_PAYLOAD = new NotifyErrorQueuePayload(TPP_DTO,MESSAGE_DTO);
+    public static final NotifyErrorQueuePayload NOTIFIER_ERROR_PAYLOAD = new NotifyErrorQueuePayload(TPP_DTO,MESSAGE_DTO);
     public static final String MESSAGE_URL = "/message";
     public static final String AUTHENTICATION_URL = "/auth";
     public static final long RETRY = 1L;
