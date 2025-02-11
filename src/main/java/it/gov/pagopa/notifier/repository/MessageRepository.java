11adbd9dc3415af47289796089c0b98f8db3dc7f
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-02-11 11:43:21 +0100

    fix: added check if there is already a message with a specific entityId an… (#18)
    Co-authored-by: BaldiVi <vittorio.baldi@emea.nttdata.com>
    Co-authored-by: andreaV95 <157486351+Vitolo-Andrea@users.noreply.github.com>
    Co-authored-by: Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>

--- src/main/java/it/gov/pagopa/notifier/repository/MessageRepository.java
+++ src/main/java/it/gov/pagopa/notifier/repository/MessageRepository.java
@@ -5,8 +5,11 @@ package it.gov.pagopa.notifier.repository;
 import it.gov.pagopa.notifier.model.Message;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface MessageRepository extends ReactiveMongoRepository<Message,String> {
     Flux<Message> findByRecipientIdAndEntityId(String recipientId, String entityId);
+
+    Mono<Message> findByMessageIdAndEntityIdrAndRecipientId(String messageId, String entityId, String recipientId);
 
 }
