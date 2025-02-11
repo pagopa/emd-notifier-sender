c2bff9b4f0529029f909c0b5650c21adaf20d691
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-02-11 16:32:32 +0100

    Update MessageService and MessageRepository

--- src/test/java/it/gov/pagopa/notifier/service/MessageServiceTest.java
+++ src/test/java/it/gov/pagopa/notifier/service/MessageServiceTest.java
@@ -41,45 +41,45 @@ class MessageServiceTest {
     @Autowired
     MessageServiceImpl messageService;
 
-//    @Test
-//    void sendMessage_MessageAlreadySent()  {
-//        Mockito.when(citizenService.getCitizenConsentsEnabled(any()))
-//                .thenReturn(Mono.just(TPP_ID_STRING_LIST));
-//
-//        Mockito.when(tppService.getTppsEnabled(any()))
-//                .thenReturn(Mono.just(TPP_DTO_LIST));
-//
-//        Mockito.when(sendNotificationService.sendNotify(MESSAGE_DTO,TPP_DTO,0))
-//                .thenReturn(Mono.empty());
-//
-//        Mockito.when(messageRepository.findByMessageIdAndEntityId(anyString(),anyString()))
-//                .thenReturn(Mono.just(MESSAGE));
-//
-//       messageService.processMessage(MESSAGE_DTO,0).block();
-//       verify(messageCoreProducerService,times(0)).enqueueMessage(MESSAGE_DTO,0);
-//       verify(messageRepository,times(1)).findByMessageIdAndEntityId(any(),any());
-//       verify(sendNotificationService,times(0)).sendNotify(MESSAGE_DTO,TPP_DTO,0);
-//    }
-//
-//    @Test
-//    void sendMessage_Ok()  {
-//        Mockito.when(citizenService.getCitizenConsentsEnabled(any()))
-//                .thenReturn(Mono.just(TPP_ID_STRING_LIST));
-//
-//        Mockito.when(tppService.getTppsEnabled(any()))
-//                .thenReturn(Mono.just(TPP_DTO_LIST));
-//
-//        Mockito.when(sendNotificationService.sendNotify(MESSAGE_DTO,TPP_DTO,0))
-//                .thenReturn(Mono.empty());
-//
-//        Mockito.when(messageRepository.findByMessageIdAndEntityId(anyString(),anyString()))
-//                .thenReturn(Mono.empty());
-//
-//        messageService.processMessage(MESSAGE_DTO,0).block();
-//        verify(messageCoreProducerService,times(0)).enqueueMessage(MESSAGE_DTO,0);
-//        verify(messageRepository,times(1)).findByMessageIdAndEntityId(any(),any());
-//        verify(sendNotificationService,times(1)).sendNotify(MESSAGE_DTO,TPP_DTO,0);
-//    }
+    @Test
+    void sendMessage_MessageAlreadySent()  {
+        Mockito.when(citizenService.getCitizenConsentsEnabled(any()))
+                .thenReturn(Mono.just(TPP_ID_STRING_LIST));
+
+        Mockito.when(tppService.getTppsEnabled(any()))
+                .thenReturn(Mono.just(TPP_DTO_LIST));
+
+        Mockito.when(sendNotificationService.sendNotify(MESSAGE_DTO,TPP_DTO,0))
+                .thenReturn(Mono.empty());
+
+        Mockito.when(messageRepository.findByMessageIdAndEntityId(anyString(),anyString()))
+                .thenReturn(Mono.just(MESSAGE));
+
+       messageService.processMessage(MESSAGE_DTO,0).block();
+       verify(messageCoreProducerService,times(0)).enqueueMessage(MESSAGE_DTO,0);
+       verify(messageRepository,times(1)).findByMessageIdAndEntityId(any(),any());
+       verify(sendNotificationService,times(0)).sendNotify(MESSAGE_DTO,TPP_DTO,0);
+    }
+
+    @Test
+    void sendMessage_Ok()  {
+        Mockito.when(citizenService.getCitizenConsentsEnabled(any()))
+                .thenReturn(Mono.just(TPP_ID_STRING_LIST));
+
+        Mockito.when(tppService.getTppsEnabled(any()))
+                .thenReturn(Mono.just(TPP_DTO_LIST));
+
+        Mockito.when(sendNotificationService.sendNotify(MESSAGE_DTO,TPP_DTO,0))
+                .thenReturn(Mono.empty());
+
+        Mockito.when(messageRepository.findByMessageIdAndEntityId(anyString(),anyString()))
+                .thenReturn(Mono.empty());
+
+        messageService.processMessage(MESSAGE_DTO,0).block();
+        verify(messageCoreProducerService,times(0)).enqueueMessage(MESSAGE_DTO,0);
+        verify(messageRepository,times(1)).findByMessageIdAndEntityId(any(),any());
+        verify(sendNotificationService,times(1)).sendNotify(MESSAGE_DTO,TPP_DTO,0);
+    }
 
     @Test
     void sendMessage_NoChannelEnabled_Case_NoConsents()  {
