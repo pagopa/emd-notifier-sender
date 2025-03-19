09850988a9cf591047ff7a49f4f67fbd1141fb94
stefanodel <stefano11.delia@nttdata.com>
2025-03-19 11:28:26 +0100

    fix: update message annotation on model

--- src/main/java/it/gov/pagopa/notifier/model/Message.java
+++ src/main/java/it/gov/pagopa/notifier/model/Message.java
@@ -1,22 +1,20 @@
 package it.gov.pagopa.notifier.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import it.gov.pagopa.notifier.dto.BaseMessage;
 import it.gov.pagopa.notifier.enums.Channel;
 import it.gov.pagopa.notifier.enums.MessageState;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 
 @EqualsAndHashCode(callSuper = true)
 @SuperBuilder
 @Data
-@Document(collection = "message")
 public class Message extends BaseMessage {
 
-    @JsonAlias("_id")
+    @Field("_id")
     private String id;
     private String entityId;
     private Channel channel;
