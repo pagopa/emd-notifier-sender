fad006f109e836a4664cdada0280ed5a544864c8
BaldiVi <vittorio.baldi@emea.nttdata.com>
2025-04-07 10:54:23 +0200

    test batch

--- src/main/java/it/gov/pagopa/notifier/EmdNotifierSender.java
+++ src/main/java/it/gov/pagopa/notifier/EmdNotifierSender.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.notifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "it.gov.pagopa")
 @EnableFeignClients(basePackages = "it.gov.pagopa.notifier.connector")
