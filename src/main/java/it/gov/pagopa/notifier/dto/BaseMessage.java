7838aef7cc97ddce731fd9d3ca4f0eca2d5a1e0f
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-07-08 16:28:50 +0200

    Normalize triggerDateTime format to be compatible with LocalDateTime (remove Z / timezone)

--- src/main/java/it/gov/pagopa/notifier/dto/BaseMessage.java
+++ src/main/java/it/gov/pagopa/notifier/dto/BaseMessage.java
@@ -6,6 +6,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 
 @AllArgsConstructor
 @Data
@@ -27,7 +32,7 @@ public class BaseMessage {
         return BaseMessage.builder()
                 .messageId(messageDTO.getMessageId())
                 .recipientId(messageDTO.getRecipientId())
-                .triggerDateTime(messageDTO.getTriggerDateTime())
+                .triggerDateTime(normalizeToLocalDateTimeFormat(messageDTO.getTriggerDateTime()))
                 .senderDescription(messageDTO.getSenderDescription())
                 .messageUrl(messageDTO.getMessageUrl())
                 .originId(messageDTO.getOriginId())
@@ -36,6 +41,22 @@ public class BaseMessage {
                 .idPsp(messageDTO.getIdPsp())
                 .notes(messageDTO.getNotes() != null ? messageDTO.getNotes() : note)
                 .build();
+    }
+    
+    private static String normalizeToLocalDateTimeFormat(String inputDateTime) {
+        try {
+            OffsetDateTime offsetDateTime = OffsetDateTime.parse(inputDateTime);
+            LocalDateTime localDateTime = offsetDateTime.toLocalDateTime();
+
+            return localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        } catch (DateTimeParseException e1) {
+            try {
+                LocalDateTime localDateTime = LocalDateTime.parse(inputDateTime);
+                return localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            } catch (DateTimeParseException e2) {
+                throw new IllegalArgumentException("Formato data non riconosciuto: " + inputDateTime, e2);
+            }
+        }
     }
 
 
